5d28fdf0af6b1d7051eb228b944429938a5ddaae
Aodhagan Murphy <aodhagan.murphy@magellanrobotech.com>
2020-06-05 20:56:50 +0100

    Updates

---
+++ precache-manifest.7dd20431d4338ce95ea782848ea41df6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc4c24441e8815af1a3b5691f7c5106",
+    "url": "/top-trumps/index.html"
+  },
+  {
+    "revision": "51eb9a40be014473a417",
+    "url": "/top-trumps/static/css/main.44026de0.chunk.css"
+  },
+  {
+    "revision": "a93f563347dfce51f8c8",
+    "url": "/top-trumps/static/js/2.2a5780ed.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/top-trumps/static/js/2.2a5780ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51eb9a40be014473a417",
+    "url": "/top-trumps/static/js/main.dc13dbf9.chunk.js"
+  },
+  {
+    "revision": "a4ce64c2a7a0659ebea0",
+    "url": "/top-trumps/static/js/runtime-main.0008a893.js"
+  }
+]);
