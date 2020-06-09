9b2cc5d1e7edd07dac778474bbe3bae2608adb22
Aodhagan Murphy <aodhagan.murphy@magellanrobotech.com>
2020-06-09 03:51:46 +0100

    creating app global styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 
-import "./reset.scss";
-import "./index.scss";
 import { theme as defaultTheme } from "./styles";
 import { GameBoardContainer } from "./components/container";
 import { GameStateProvider } from "./state";
+import AppGlobalStyles from "./AppGlobalStyles";
 
 const App = () => (
 	<ThemeProvider theme={defaultTheme}>
+		<AppGlobalStyles />
 		<GameStateProvider>
 			<GameBoardContainer />
 		</GameStateProvider>
